179aef59ade5334a556c2ff29ffbea9d931b8df7
Mohammed Yaqob <mohammed.yaqob.98@gmail.com>
2021-08-29 06:14:37 +0200

    Initial Commit

---
+++ Quizzler/Model/Question.swift
@@ -0,0 +1,20 @@
+//
+//  Question.swift
+//  Quizzler
+//
+//  Created by Mohammed Yaqob's MacBook on 02/09/2020.
+//
+
+import Foundation
+
+struct Question {
+    let text: String
+    let answer: String
+    
+    init(q: String, a: String) {
+        text = q
+        answer = a
+    }
+}
+
+    
